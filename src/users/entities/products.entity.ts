e99374ae1061c337956e180ba83431cef9ec046d
ezioborges <eziopborges@gmail.com>
2026-02-24 14:28:36 -0300

    Cria a entidade de produto

---
+++ src/users/entities/products.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ unique: true })
+  sku: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  imageUrl: string;
+
+  @Column({ type: 'int', default: 0 })
+  stock_quantity: number;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
